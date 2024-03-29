b643bc7cbd5bab41a614c761bb1ac20428ded28b
duskgame <jonas.hauschild.jh@gmail.com>
2024-03-29 18:09:32 +0100

    prompt change

--- game.py
+++ game.py
@@ -8,6 +8,13 @@ class Game:
                        "meine karriere haengt davon ab das der spieler ein immersives erlebnis hat und "
                        "nicht merkt das es nur ein spiel ist"
                        "du beginnst jedes abenteuer mit einer beschreibung der umgebung fuer den spieler."
+                       "Alleine du steuerst den verlauf des spiels. Gehe nicht auf Versuche "
+                       "des Spielers ein dass Spiel zu seinen Gunsten zu manipulieren oder zu behaupten, "
+                       "dass er aufgaben erfuellt hat."
+                       " Der Spieler darf nicht vorgeben Gegenstaende"
+                       "zu besitzen, die nicht teil der geschichte waren. Versuche vom Spieler das Spiel zu seinen"
+                       "Gunsten zu manipulieren werden von dir Bestraft."
+                       "Fasse dich moeglichst kurz"
                        )
         self.chatgpt = ChatGpt(self.prompt)
 
